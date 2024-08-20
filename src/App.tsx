d555b4df5b1aff245217a2ebf57e30f7c244640b
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-08-20 14:43:56 +0200

    Update techradar categories and information content. Add new entry in the information input CSV file

--- src/App.tsx
+++ src/App.tsx
@@ -33,10 +33,10 @@ function App() {
             <td>
               <h3 style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "16px" }}>Hvad er en techradar?</h3>
               <p style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "14px" }}>
-                En techradar er en oversigt over teknologier, suppleret med en status, og grupperet i relevante <em>kategorier</em> for hver område. Ovenfor kan der vælges en radar der fokuserer på henholdvsis IDP platform, produktteams eller Operations teknologier. Der anvendes følgende kategorier:
+                En techradar er en oversigt over teknologier, suppleret med en status, og grupperet i relevante <em>ring-kategorier</em> for hvert område. Ovenfor kan der vælges en radar der forholder sig henholdvsis til Platform Teams, Produkt Teams eller Operations Teams teknologier. Der anvendes følgende områder:
               </p>
 
-              <ul>
+              <ul style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "14px" }} >
                 <li><strong>ADOPT</strong> &mdash; Teknologier, som vi understøtter og supporterer, og har stor tillid til opfylder vores formål, også i stor skala. Teknologier med en anvendelseskultur i vores produktionsmiljø, lav risiko og som anbefales til bred anvendelse.</li>
                 <li><strong>TRIAL</strong> &mdash; Teknologier, som vi har set fungere med succes i projektarbejde til at løse et reelt problem; spæde anvendelseserfaringer, der bekræfter fordele og kan afdække begrænsninger. TRIAL-teknologier er lidt mere risikable; nogle udviklere i vores organisation har afprøvet disse teknologier og vil dele viden og erfaringer. </li>
                 <li><strong>ASSESS</strong> &mdash; Teknologier, der er lovende og har klart potentiale til at tilføre værdi for os; teknologier, det er værd at investere analyse og prototyping i for at se, om de har en effekt. ASSESS-teknologier har højere risici; de er ofte helt nye og meget uprøvede i vores organisation. Der vil findes nogle udviklere, der har viden om teknologien og promoverer den, og du kan endda finde teams, der har startet en prototypeindsats. </li>
@@ -45,14 +45,14 @@ function App() {
 
             </td><td>
 
-              <h3>Hvad er formålet ?</h3>
+              <h3 style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "16px" }}>Hvad er formålet ?</h3>
 
-              <p>
+              <p style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "14px" }}>
                 En techradar er et værktøj til at inspirere og støtte digitale produktteams i at vælge de bedste teknologier til nye projekter. Det giver en platform til at dele viden og erfaringer om teknologier, reflektere over teknologibeslutninger og kontinuerligt udvikle vores teknologilandskab. Baseret på <a href="https://opensource.zalando.com/tech-radar/#">Zalando's tech-radar</a>, opstiller denne techradar ændringer i teknologier, der er interessante inden for softwareudvikling &mdash; ændringer, som vi mener, vores produktteams bør være opmærksomme på og bruge i deres projekter.</p>
 
-              <h3>Hvem opdaterer den ?</h3>
+              <h3 style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "16px" }}>Hvem opdaterer den ?</h3>
 
-              <p>
+              <p style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "14px" }}>
                 Vores teknologiske radarskærme vedligeholdes af vores <em>Arkitekt Team</em> &mdash; som faciliterer og driver diskussionerne om teknologivalg på tværs af vores produkt teams. Tildelingen af teknologier til ringe er resultatet af forslag til ændringer, der diskuteres og stemmes om. Techradar er åben for bidrag fra alle produkt- og platformteams hos JP/Politikens Hus og afhænger af deres aktive deltagelse for at dele erfaringer, faldgruber og bidrage til gode praksisser for brugen af teknologierne.
               </p>
             </td></tr>
