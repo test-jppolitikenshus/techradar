fa168d3c145b31fa5fdaabcc6b4f3f99d1acff81
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-08-20 14:30:03 +0200

    Update techradar descriptions and clarify usage of categories in App component

--- src/App.tsx
+++ src/App.tsx
@@ -33,13 +33,13 @@ function App() {
             <td>
               <h3 style={{ fontFamily: "Geneva, Arial, Helvetica, sans-serif", fontSize: "14px", color: "rgb(153, 153, 153)" }}>Hvad er en techradar?</h3>
               <p>
-                En techradar er en oversigt over teknologier, suppleret med en status, og fordelt på <em>kategori</em>. I ovenstående anvendes følgende kategorier(ringe):
+                En techradar er en oversigt over teknologier, suppleret med en status, og grupperet i relevante <em>kategorier</em> for hver område. Ovenfor kan der vælges en radar der fokuserer på henholdvsis IDP platform, produktteams eller Operations teknologier. Der anvendes følgende kategorier:
               </p>
 
               <ul>
                 <li><strong>ADOPT</strong> &mdash; Teknologier, som vi understøtter og supporterer, og har stor tillid til opfylder vores formål, også i stor skala. Teknologier med en anvendelseskultur i vores produktionsmiljø, lav risiko og som anbefales til bred anvendelse.</li>
                 <li><strong>TRIAL</strong> &mdash; Teknologier, som vi har set fungere med succes i projektarbejde til at løse et reelt problem; spæde anvendelseserfaringer, der bekræfter fordele og kan afdække begrænsninger. TRIAL-teknologier er lidt mere risikable; nogle udviklere i vores organisation har afprøvet disse teknologier og vil dele viden og erfaringer. </li>
-                <li><strong>ASSESS</strong> &mdash; Teknologier, der er lovende og har klart potentiale til at tilføre værdi for os; teknologier, det er værd at investere analyse og prototyping i for at se, om de har en effekt. ASSESS-teknologier har højere risici; de er ofte helt nye og meget uprøvede i vores organisation. Du vil finde nogle udviklere, der har viden om teknologien og promoverer den, og du kan endda finde teams, der har startet en prototypeindsats. </li>
+                <li><strong>ASSESS</strong> &mdash; Teknologier, der er lovende og har klart potentiale til at tilføre værdi for os; teknologier, det er værd at investere analyse og prototyping i for at se, om de har en effekt. ASSESS-teknologier har højere risici; de er ofte helt nye og meget uprøvede i vores organisation. Der vil findes nogle udviklere, der har viden om teknologien og promoverer den, og du kan endda finde teams, der har startet en prototypeindsats. </li>
                 <li><strong>HOLD</strong> &mdash; Teknologier, der ikke anbefales til brug i nye projekter. Teknologier, som vi ikke mener er værd at investere yderligere i. HOLD-teknologier bør ikke anvendes i nye projekter, men kan normalt fortsættes i eksisterende projekter.</li>
               </ul>
 
@@ -48,7 +48,7 @@ function App() {
               <h3>Hvad er formålet ?</h3>
 
               <p>
-                En techradar er et værktøj til at inspirere og støtte digitale produktteams i at vælge de bedste teknologier til nye projekter; det giver en platform til at dele viden og erfaringer om teknologier, reflektere over teknologibeslutninger og kontinuerligt udvikle vores teknologilandskab. Baseret på <a href="https://opensource.zalando.com/tech-radar/#">Zalandos arbejde</a>, opstiller denne techradar ændringer i teknologier, der er interessante inden for softwareudvikling &mdash; ændringer, som vi mener, vores produktteams bør være opmærksomme på og bruge i deres projekter.</p>
+                En techradar er et værktøj til at inspirere og støtte digitale produktteams i at vælge de bedste teknologier til nye projekter. Det giver en platform til at dele viden og erfaringer om teknologier, reflektere over teknologibeslutninger og kontinuerligt udvikle vores teknologilandskab. Baseret på <a href="https://opensource.zalando.com/tech-radar/#">Zalando's tech-radar</a>, opstiller denne techradar ændringer i teknologier, der er interessante inden for softwareudvikling &mdash; ændringer, som vi mener, vores produktteams bør være opmærksomme på og bruge i deres projekter.</p>
 
               <h3>Hvem opdaterer den ?</h3>
 
