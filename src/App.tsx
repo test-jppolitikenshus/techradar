dec11dccf3e79a5837ed0b1f85ab56328d806b6f
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2024-08-20 13:40:06 +0200

    Add new feature data source for techradar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Techradar, { TechradarData } from '../lib/main'
 import idp from './techradarIdp';
 import dut from './techradarDut';
 import pub from './techradarInf';
+import feat from './techradarFeat';
 
 function App() {
   const [example, setExample] = useState<TechradarData>(idp);
@@ -30,30 +31,30 @@ function App() {
         <tbody>
           <tr>
             <td>
-              <h3>What is the techradar?</h3>
+              <h3>Hvad er en techradar?</h3>
               <p>
                 The techradar is a list of technologies, complemented by an assessment result, called <em>ring assignment</em>. In this example the following rings are used:
+                En techradar er en oversigt over teknologier, suppleret med en status, kaldet <em>ringtildeling</em>. I dette eksempel anvendes følgende ringe:
               </p>
 
               <ul>
-                <li><strong>ADOPT</strong> &mdash; Technologies we have high confidence in to serve our purpose, also in large scale. Technologies with a usage culture in our production environment, low risk and recommended to be widely used.</li>
-                <li><strong>TRIAL</strong> &mdash; Technologies that we have seen work with success in project work to solve a real problem; first serious usage experience that confirm benefits and can uncover limitations. TRIAL technologies are slightly more risky; some engineers in our organization walked this path and will share knowledge and experiences.</li>
-                <li><strong>ASSESS</strong> &mdash; Technologies that are promising and have clear potential value-add for us; technologies worth to invest some research and prototyping efforts in to see if it has impact. ASSESS technologies have higher risks; they are often brand new and highly unproven in our organisation. You will find some engineers that have knowledge in the technology and promote it, you may even find teams that have started a prototyping effort.</li>
-                <li><strong>HOLD</strong> &mdash; Technologies not recommended to be used for new projects. Technologies that we think are not (yet) worth to (further) invest in. HOLD technologies should not be used for new projects, but usually can be continued for existing projects.</li>
+                <li><strong>ADOPT</strong> &mdash; Teknologier, som vi understøtter og supporterer, og har stor tillid til opfylder vores formål, også i stor skala. Teknologier med en anvendelseskultur i vores produktionsmiljø, lav risiko og som anbefales til bred anvendelse.</li>
+                <li><strong>TRIAL</strong> &mdash; Teknologier, som vi har set fungere med succes i projektarbejde til at løse et reelt problem; spæde anvendelseserfaringer, der bekræfter fordele og kan afdække begrænsninger. TRIAL-teknologier er lidt mere risikable; nogle udviklere i vores organisation har afprøvet disse teknologier og vil dele viden og erfaringer. </li>
+                <li><strong>ASSESS</strong> &mdash; Teknologier, der er lovende og har klart potentiale til at tilføre værdi for os; teknologier, det er værd at investere analyse og prototyping i for at se, om de har en effekt. ASSESS-teknologier har højere risici; de er ofte helt nye og meget uprøvede i vores organisation. Du vil finde nogle udviklere, der har viden om teknologien og promoverer den, og du kan endda finde teams, der har startet en prototypeindsats. </li>
+                <li><strong>HOLD</strong> &mdash; Teknologier, der ikke anbefales til brug i nye projekter. Teknologier, som vi ikke mener er værd at investere yderligere i. HOLD-teknologier bør ikke anvendes i nye projekter, men kan normalt fortsættes i eksisterende projekter.</li>
               </ul>
 
             </td><td>
 
-              <h3>What is the purpose?</h3>
+              <h3>Hvad er formålet ?</h3>
 
               <p>
-                The techradar is a tool to inspire and support Engineering teams to pick the best technologies for new projects; it provides a platform to share knowledge and experience in technologies, to reflect on technology decisions and continuously evolve our technology landscape. Based on the <a href="https://opensource.zalando.com/tech-radar/#"> work of Zalando</a>, this techradar sets out the changes in technologies that are interesting in software development &mdash; changes that we think our engineering teams should pay attention to and use in their projects.
-              </p>
+                En techradar er et værktøj til at inspirere og støtte digitale produktteams i at vælge de bedste teknologier til nye projekter; det giver en platform til at dele viden og erfaringer om teknologier, reflektere over teknologibeslutninger og kontinuerligt udvikle vores teknologilandskab. Baseret på <a href="https://opensource.zalando.com/tech-radar/#">udarbejdet af Zalando</a>, opstiller denne techradar ændringer i teknologier, der er interessante inden for softwareudvikling &mdash; ændringer, som vi mener, vores produktteams bør være opmærksomme på og bruge i deres projekter.</p>
 
-              <h3>How do we maintain it?</h3>
+              <h3>Hvem opdaterer den ?</h3>
 
               <p>
-                The techradar is maintained by our <em>Principal Engineers</em> &mdash; who facilitate and drive the technology selection discussions across the Engineering Community. Assignment of technologies to rings is the outcome of ring change proposals, which are discussed and voted on. The techradar is open for contribution for all Engineering teams at Zalando and depends on their active participation to share lessons learned, pitfalls, and contribute to good practices on using the technologies.<br />
+                Vores teknologiske radarskærme vedligeholdes af vores <em>Arkitekt Team</em> &mdash; som faciliterer og driver diskussionerne om teknologivalg på tværs af vores produkt teams. Tildelingen af teknologier til ringe er resultatet af forslag til ændringer, der diskuteres og stemmes om. Techradar er åben for bidrag fra alle produkt- og platformteams hos JP/Politikens Hus og afhænger af deres aktive deltagelse for at dele erfaringer, faldgruber og bidrage til gode praksisser for brugen af teknologierne.
               </p>
             </td></tr>
         </tbody>
